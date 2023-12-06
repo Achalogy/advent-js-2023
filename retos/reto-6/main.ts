e6134e2b325e1f291e1dbf77e66004e6952bfc88
Achalogy <achalogy@gmail.com>
2023-12-06 09:40:30 -0500

    Solución Reto 6

---
+++ retos/reto-6/main.ts
@@ -0,0 +1,15 @@
+function maxDistance(movements) {
+  let distance = 0
+
+  let right = movements.match(/>/g)?.length ?? 0
+  let left = movements.match(/</g)?.length ?? 0
+
+  distance += right
+  distance -= left
+
+  let extra = movements.length - (right + left)
+
+  return Math.abs(distance) + Math.abs(extra)
+}
+
+module.exports = maxDistance
