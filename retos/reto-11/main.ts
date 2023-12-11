eb0a41f7a044178da6bb897ace776c2dcaae2fa2
Achalogy <achalogy@gmail.com>
2023-12-11 09:50:36 -0500

    Mejor solución

--- retos/reto-11/main.ts
+++ retos/reto-11/main.ts
@@ -1,7 +1,7 @@
 function getIndexsForPalindrome(word) {
   let res: any = null
-  for (let a of Array.from({ length: word.length }).keys()) {
-    for (let b of Array.from({ length: word.length }).keys()) {
+  for (const a of Array.from({ length: word.length }).keys()) {
+    for (const b of Array.from({ length: word.length }).keys()) {
       let swapped = [...word]
       let aux = word[a]
       swapped[a] = word[b]
@@ -10,15 +10,17 @@ function getIndexsForPalindrome(word) {
       let left = swapped.slice(0, Math.floor(word.length / 2)).join("")
       let right = swapped.slice(Math.ceil(word.length / 2)).reverse().join("")
 
-      if (left == right) {
-        res = [[], [a, b]][+((a + b) > 0)]
-        return res;
-      }
+      res = [
+        [
+          null
+          , [
+            []
+            , [a, b]
+          ][+((a + b) > 0)]
+        ][+(left == right)]
+        , res
+      ][+!!res]
     }
   }
   return res
 }
-
-console.log(
-  getIndexsForPalindrome('abab')
-)
